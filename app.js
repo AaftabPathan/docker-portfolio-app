032053cfed44b478ce720f02185379e7bb5a29b9
Aaftab Pathan <aaftabaayubpathan@gmail.com.com>
2026-04-16 12:54:24 +0000

    fix: eslint issues

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome to DevOps App 🚀');
+app.get("/", (req, res) => {
+  res.send("Welcome to DevOps App 🚀");
 });
 
-app.get('/api', (req, res) => {
-    res.json({ message: "Hello from backend" });
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from backend" });
 });
 
-app.get('/health', (req, res) => {
-    res.json({ status: "UP" });
+app.get("/health", (req, res) => {
+  res.json({ status: "UP" });
 });
 
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
 
 const port = process.env.PORT || 3000;
