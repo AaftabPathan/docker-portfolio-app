12a48100ab4f19ee6d547c9054ff2479f46e2209
Aaftab Pathan <aaftabaayubpathan@gmail.com.com>
2026-04-19 07:37:00 +0000

    add eslint config

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,14 +1,10 @@
-export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module"
-    },
-    rules: {
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
-      indent: ["error", 2]
-    }
-  }
-];
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  tseslint.configs.recommended,
+]);
